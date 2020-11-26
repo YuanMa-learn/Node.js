a55fbdd79136a31f285d8a6d13a44bca3cb78b11
YuanMa-learn <15662728844@163.com>
2020-11-26 10:35:53 +0800

    服务器的开启及文件的读写

--- 文件读取和web服务器的构建/fileRead.js
+++ 文件读取和web服务器的构建/fileRead.js
@@ -2,6 +2,9 @@ var http = require ('http')
 var fs = require ('fs')
 http.createServer (function (req, res){
     console.log('请求的路径为' + req.url)
+    // 设置响应数据格式和编码方式，只有字符串类型才需要设置编码方式
+    res.setHeader('Content-type', 'text/plain;charset=utf-8')
+    res.write('hello 您好')
     // 写文件 参数（文件路径，写入字符串,处理函数）
     fs.writeFile('../file.txt', 'hello node.js', function (error){
         if (error){
